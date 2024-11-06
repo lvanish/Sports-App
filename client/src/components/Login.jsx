b5cbf6dc93e6a636d457eea513102d148afb0aef
Lvanish Koul <lvanishkoul@gmail.com>
2024-11-06 14:45:03 +0530

    Update Login.jsx

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -27,7 +27,7 @@ const Login = ({ setUser }) => {
 
   const handleRegister = async (e) => {
     e.preventDefault();
-    const response = await fetch("http://localhost:4000/register", {
+    const response = await fetch("https://sports-app-nyuc-backend.onrender.com/register", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ email, password }),
